1a3b2c375e7a0dc86fd0bc9882eae1ae740b5ea4
Joacim Magnusson <joacim@isogram.se>
2025-01-21 22:31:42 +0100

    Add show/hide toggle to generate panel

--- src/app.ts
+++ src/app.ts
@@ -145,6 +145,8 @@ export class MyApp extends LitElement {
 
   @state() private _loading = false;
 
+  @state() private _showAI = false;
+
   @query("#renderTarget")
   private _renderTarget!: HTMLElement;
 
@@ -163,6 +165,13 @@ export class MyApp extends LitElement {
     document.addEventListener("preview-tree-updated", () =>
       this.#renderPreviewDocument(),
     );
+    document.addEventListener("keydown", (e: KeyboardEvent) => {
+      // if command slash
+      // toggle ai
+      if (e.metaKey && e.key === "/") {
+        this._showAI = !this._showAI;
+      }
+    });
   }
 
   render() {
@@ -231,39 +240,42 @@ export class MyApp extends LitElement {
                 style="margin-left:auto; margin-right:auto; margin-bottom: 1rem; font-size: 3rem; --indicator-color: #333; --track-color: #bbb;--track-width: 10px;"
               ></sl-spinner>`,
           )}
-
-          <gds-flex flex="0 1 100px" border="4xs 0 0 0" padding="m">
-            <form
-              style="width: 100%"
-              @submit=${async (e: SubmitEvent) => {
-                e.preventDefault();
-                this._loading = true;
-                const form = new FormData(e.target as HTMLFormElement);
-                const response = await fetch("/api/generate", {
-                  method: "POST",
-                  body: JSON.stringify({ message: form.get("generate") }),
-                });
-                const responseJson = await response.json();
-                this._loading = false;
-                try {
-                  const json = JSON.parse(responseJson.reply.content);
-                  this._document = elementFactory(json);
-                } catch (e) {
-                  console.error(e);
-                }
-              }}
-            >
-              <gds-input
-                label="Generate"
-                name="generate"
-                @keydown=${(e: KeyboardEvent) => {
-                  if (e.key === "Enter") {
-                    (e.target as any).form.requestSubmit();
-                  }
-                }}
-              ></gds-input>
-            </form>
-          </gds-flex>
+          ${when(
+            this._showAI,
+            () =>
+              html`<gds-flex flex="0 1 100px" border="4xs 0 0 0" padding="m">
+                <form
+                  style="width: 100%"
+                  @submit=${async (e: SubmitEvent) => {
+                    e.preventDefault();
+                    this._loading = true;
+                    const form = new FormData(e.target as HTMLFormElement);
+                    const response = await fetch("/api/generate", {
+                      method: "POST",
+                      body: JSON.stringify({ message: form.get("generate") }),
+                    });
+                    const responseJson = await response.json();
+                    this._loading = false;
+                    try {
+                      const json = JSON.parse(responseJson.reply.content);
+                      this._document = elementFactory(json);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}
+                >
+                  <gds-input
+                    label="Generate"
+                    name="generate"
+                    @keydown=${(e: KeyboardEvent) => {
+                      if (e.key === "Enter") {
+                        (e.target as any).form.requestSubmit();
+                      }
+                    }}
+                  ></gds-input>
+                </form>
+              </gds-flex>`,
+          )}
         </gds-flex>
         <gds-flex
           flex="0 0 300px"
