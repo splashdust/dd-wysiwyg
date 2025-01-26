5e0da0ea9f4d1b1c2201f1214b3ba6ee83b6cfc9
Joacim Magnusson <joacim@isogram.se>
2025-01-26 21:43:48 +0100

    Fix alignment of AI spinner

--- src/components/ai-generate.ts
+++ src/components/ai-generate.ts
@@ -22,49 +22,51 @@ export class AiGenerate extends LitElement {
   }
 
   render() {
-    return html` ${when(
-      this._loading,
-      () =>
-        html`<sl-spinner
-          style="margin-left:auto; margin-right:auto; margin-bottom: 1rem; font-size: 3rem; --indicator-color: #333; --track-color: #bbb;--track-width: 10px;"
-        ></sl-spinner>`,
-    )}
-    ${when(
-      this._showAI,
-      () =>
-        html`<gds-flex flex="0 1 100px" border="4xs 0 0 0" padding="m">
-          <form
-            style="width: 100%"
-            @submit=${async (e: SubmitEvent) => {
-              e.preventDefault();
-              this._loading = true;
-              const form = new FormData(e.target as HTMLFormElement);
-              const response = await fetch("/api/generate", {
-                method: "POST",
-                body: JSON.stringify({ message: form.get("generate") }),
-              });
-              const responseJson = await response.json();
-              this._loading = false;
-              try {
-                const json = JSON.parse(responseJson.reply.content);
-                edDocument.root = elementFactory(json);
-              } catch (e) {
-                console.error(e);
-              }
-            }}
-          >
-            <gds-textarea
-              label="Generate"
-              name="generate"
-              @keydown=${(e: KeyboardEvent) => {
-                if (e.key === "Enter" && !e.shiftKey) {
-                  e.preventDefault();
-                  (e.target as any).form.requestSubmit();
+    return html`<gds-flex>
+        ${when(
+          this._loading,
+          () =>
+            html`<sl-spinner
+              style="margin-left:auto; margin-right:auto; margin-bottom: 1rem; font-size: 3rem; --indicator-color: #333; --track-color: #bbb;--track-width: 10px;"
+            ></sl-spinner>`,
+        )}
+      </gds-flex>
+      ${when(
+        this._showAI,
+        () =>
+          html`<gds-flex flex="0 1 100px" border="4xs 0 0 0" padding="m">
+            <form
+              style="width: 100%"
+              @submit=${async (e: SubmitEvent) => {
+                e.preventDefault();
+                this._loading = true;
+                const form = new FormData(e.target as HTMLFormElement);
+                const response = await fetch("/api/generate", {
+                  method: "POST",
+                  body: JSON.stringify({ message: form.get("generate") }),
+                });
+                const responseJson = await response.json();
+                this._loading = false;
+                try {
+                  const json = JSON.parse(responseJson.reply.content);
+                  edDocument.root = elementFactory(json);
+                } catch (e) {
+                  console.error(e);
                 }
               }}
-            ></gds-textarea>
-          </form>
-        </gds-flex>`,
-    )}`;
+            >
+              <gds-textarea
+                label="Generate"
+                name="generate"
+                @keydown=${(e: KeyboardEvent) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    (e.target as any).form.requestSubmit();
+                  }
+                }}
+              ></gds-textarea>
+            </form>
+          </gds-flex>`,
+      )}`;
   }
 }
